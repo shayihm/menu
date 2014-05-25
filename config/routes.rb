f472dc8e820489c29fb8f629e45ef2da524d9382
shayihm <shayihm@gmail.com>
2014-05-24 23:25:39 -0400

    created menu scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :recipes
+
   devise_for :users
   root to: "welcome#home"
   # The priority is based upon order of creation: first created -> highest priority.
